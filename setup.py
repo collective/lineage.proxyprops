016115f3a43774867444ff6fc444b7ff30643524
Clayton Parker <me@claytron.com>
2009-05-30 15:56:41 +0000

    removing paster plugin stuff since that seemed to cause some issues, and we don't need it

--- setup.py
+++ setup.py
@@ -29,14 +29,5 @@ setup(name='lineage.proxyprops',
           'collective.lineage',
           'collective.proxyproperties'
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-
-      [distutils.setup_keywords]
-      paster_plugins = setuptools.dist:assert_string_list
-
-      [egg_info.writers]
-      paster_plugins.txt = setuptools.command.egg_info:write_arg
-      """,
-      paster_plugins = ["ZopeSkel"],
+      entry_points="",
       )
